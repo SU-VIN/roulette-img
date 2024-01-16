44336edc606835d74aab294ad501c3cf68f194e5
LIM-SUVIN <pyotnqls25@naver.com>
2024-01-16 11:35:07 +0900

    refactor: 타입추가 및 props추가

--- src/lib/types/roulette.ts
+++ src/lib/types/roulette.ts
@@ -3,10 +3,15 @@ type chunkRange = {
   end: number;
 };
 
+export type buttonShape = "round" | "squre";
+
 export interface Roulette {
   imgUrl: string;
+  arrowImgUrl: string;
   chunkRange: chunkRange;
   chunk?: number;
   arrowPosition?: string;
   winNumber?: number;
+  buttonText?: string;
+  buttonShape?: buttonShape;
 }
